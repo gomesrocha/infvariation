b4d05381795d024124f05dedaa44a0b5f7605d1f
Fabio Gomes Rocha <gomesrocha@gmail.com>
2024-08-20 16:32:48 -0300

    modify version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="infvariation",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python library for generating information variations, such as typo-squatting domains, leet transformations, and custom text variations.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
@@ -10,6 +10,9 @@ setup(
     author_email="gomesrocha@gmail.com",
     url="https://github.com/gomesrocha/infvariation",
     packages=find_packages(),
+    install_requires=[
+        "ail_typo_squatting",
+        "leet",    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         'License :: OSI Approved :: Apache Software License',
